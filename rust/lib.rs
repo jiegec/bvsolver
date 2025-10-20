12f45c1d8b263b5319d5298d06db4a52177bb6df
Jiajie Chen <c@jia.je>
2025-10-21 03:23:02 +0800

    Use biguint

--- rust/lib.rs
+++ rust/lib.rs
@@ -1,17 +1,20 @@
 #[pyo3::pymodule]
 mod _lib {
-    use num_bigint::{BigInt, ToBigInt};
+    use num_bigint::{BigInt, BigUint, ToBigInt, ToBigUint};
     use pyo3::prelude::*;
     use pyo3::types::PyInt;
 
     #[pyfunction]
     fn solve(size: usize, zeros: Vec<Bound<'_, PyInt>>) -> PyResult<(BigInt, Vec<BigInt>)> {
-        let zeros: Vec<BigInt> = zeros.iter().map(|num| num.extract().unwrap()).collect();
+        let zeros: Vec<BigUint> = zeros
+            .iter()
+            .map(|num| num.extract::<BigInt>().unwrap().to_biguint().unwrap())
+            .collect();
         // gauss elimination
         let mut done = 0;
         // for each bit, record its row with the bit set to 1
-        let mut mapping = vec![BigInt::ZERO; size];
-        let one = 1.to_bigint().unwrap();
+        let mut mapping = vec![BigUint::ZERO; size];
+        let one = 1.to_biguint().unwrap();
         for e in zeros {
             if e == one {
                 // no solution
@@ -22,10 +25,10 @@ mod _lib {
             let mut e = e;
             let mut added = false;
             let mut e_no_low = &e ^ (&e & &one);
-            while e_no_low != BigInt::ZERO {
+            while e_no_low != BigUint::ZERO {
                 // minus 1: 0b10 -> mapping index 0
                 let bit = e_no_low.trailing_zeros().unwrap() as usize - 1;
-                if mapping[bit] != BigInt::ZERO {
+                if mapping[bit] != BigUint::ZERO {
                     e = &e ^ &mapping[bit];
                     e_no_low = &e ^ (&e & &one);
                 } else {
@@ -36,7 +39,7 @@ mod _lib {
                 }
             }
 
-            if !added && e != BigInt::ZERO {
+            if !added && e != BigUint::ZERO {
                 // eliminate to non-zero, no solution
                 return Ok((-1.to_bigint().unwrap(), vec![]));
             }
@@ -44,8 +47,8 @@ mod _lib {
 
         // find free variables
         let mut free = vec![];
-        for i in 0..size {
-            if mapping[i] == BigInt::ZERO {
+        for (i, row) in mapping.iter().enumerate() {
+            if row == &BigUint::ZERO {
                 free.push(i);
             }
         }
@@ -60,9 +63,9 @@ mod _lib {
                 }
             }
         }
-        for i in 0..size {
+        for (i, row) in mapping.iter().enumerate() {
             // after elimination, compute solution if free variables are zero
-            base_res.set_bit(i as u64 + 1, mapping[i].bit(0));
+            base_res.set_bit(i as u64 + 1, row.bit(0));
         }
 
         // now all free variables are found
@@ -72,13 +75,13 @@ mod _lib {
         let mut contributions = vec![];
         for bit in free {
             let mut c = BigInt::ZERO;
-            for i in 0..size {
-                if mapping[i].bit((bit + 1) as u64) || i == bit {
+            for (i, row) in mapping.iter().enumerate() {
+                if i == bit || row.bit((bit + 1) as u64) {
                     c.set_bit((i + 1) as u64, true);
                 }
             }
             contributions.push(c);
         }
-        Ok((base_res, contributions))
+        Ok((base_res.to_bigint().unwrap(), contributions))
     }
 }
