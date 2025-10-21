9c9f8019ef513172879574173e8ac83726584214
Jiajie Chen <c@jia.je>
2025-10-21 11:24:20 +0800

    Handle failed solve

--- python/examples/geekgame_2024_randomzoo_task2.py
+++ python/examples/geekgame_2024_randomzoo_task2.py
@@ -14,38 +14,43 @@ with tempfile.NamedTemporaryFile("w", suffix=".py") as f:
     f.write(code)
     f.flush()
 
-    p = process(["python3", f.name])
+    # sometimes it overflows so no solution is found
+    # loop until solved
+    while True:
+        p = process(["python3", f.name])
 
-    witness = 2000
-    known = []
-    for i in range(witness):
-        known.append(int(p.recvline().decode().strip()))
-        p.sendline()
+        witness = 2000
+        known = []
+        for i in range(witness):
+            known.append(int(p.recvline().decode().strip()))
+            p.sendline()
 
-    # create equations
-    solver = bvsolver.Solver([32] * 624)
-    state = solver.bitvectors()
+        # create equations
+        solver = bvsolver.Solver([32] * 624)
+        state = solver.bitvectors()
 
-    rng = bvsolver.CPythonRandom(
-        state,
-        # newly created Random instance has its index equal to 624
-        624,
-    )
-    # drop the lowest 20 bits, assuming adding flag does not overflow to upper bits
-    shift = 20
-    # find solutions
-    for sol in solver.solve(
-        [(rng.getrandbits(32) >> shift) ^ (value >> shift) for value in known]
-        # newly created Random instance has its first element equal to 0x80000000
-        # see init_by_array in _randommodule.c
-        + [state[0] ^ 0x80000000]
-    ):
-        print("Found solution")
-        state_recover = [s.get(sol) for s in state]
+        rng = bvsolver.CPythonRandom(
+            state,
+            # newly created Random instance has its index equal to 624
+            624,
+        )
+        # drop the lowest 20 bits, assuming adding flag does not overflow to upper bits
+        shift = 20
+        # find solutions
+        for sol in solver.solve(
+            [(rng.getrandbits(32) >> shift) ^ (value >> shift) for value in known]
+            # newly created Random instance has its first element equal to 0x80000000
+            # see init_by_array in _randommodule.c
+            + [state[0] ^ 0x80000000]
+        ):
+            print("Found solution")
+            state_recover = [s.get(sol) for s in state]
 
-        # recreate random number generator
-        rng_recover = bvsolver.CPythonRandom(state_recover, 624).to_cpython_random()
-        flag = ""
-        for i in range(50):
-            flag += chr(known[i] - rng_recover.getrandbits(32))
-        print(flag)
+            # recreate random number generator
+            rng_recover = bvsolver.CPythonRandom(state_recover, 624).to_cpython_random()
+            flag = ""
+            for i in range(50):
+                flag += chr(known[i] - rng_recover.getrandbits(32))
+            print(flag)
+
+            exit(0)
