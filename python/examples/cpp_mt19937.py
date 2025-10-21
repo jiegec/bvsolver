d7617b6e109e28712381bf489224f150df1b5d8b
Jiajie Chen <c@jia.je>
2025-10-21 15:42:16 +0800

    Add cpp mt19937 recover

---
+++ python/examples/cpp_mt19937.py
@@ -0,0 +1,51 @@
+from pwn import *
+import bvsolver
+
+code = """
+#include <random>
+#include <iostream>
+int main() {
+    std::random_device rd;
+    std::mt19937 gen(rd());
+    for (int i = 0;i < 10000;i++) {
+        std::cout << gen() << std::endl;
+    }
+    return 0;
+}
+"""
+
+with tempfile.NamedTemporaryFile("w", suffix=".cpp") as f:
+    f.write(code)
+    f.flush()
+
+    os.system(f"g++ -O2 {f.name} -o {f.name}.bin")
+
+    p = process([f"{f.name}.bin"])
+    all_nums = [int(p.recvline().decode().strip()) for i in range(10000)]
+    known = all_nums[:624]
+
+    # create equations
+    solver = bvsolver.Solver([32] * 624)
+    state = solver.bitvectors()
+
+    # cpp mt19937 is same as cpython, except for its seeding
+    rng = bvsolver.CPythonRandom(
+        state,
+        624,
+    )
+
+    zeros = []
+    for value in known:
+        zeros.append(rng.getrandbits(32) ^ value)
+
+    # find solutions
+    for sol in solver.solve(zeros):
+        print("Found solution")
+        state_recover = [s.get(sol) for s in state]
+
+        # recreate random number generator
+        rng_recover = bvsolver.CPythonRandom(state_recover, 624).to_cpython_random()
+        assert all_nums == [rng_recover.getrandbits(32) for i in range(len(all_nums))]
+
+        # multiple solutions, choose one
+        break
